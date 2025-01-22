369210ea632b36e9b799876f7d6f6514f12f4968
Patrick Punch <punchp@oregonstate.edu>
2025-01-22 10:45:40 -0800

    completed tokenize2

---
+++ tokenize2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    (void)argc; // stop compiler warnings about ununsed variables
+    (void)argv;
+    char line[1024];
+
+    fgets(line, sizeof(line), stdin);
+
+    size_t str_length = strlen(line);
+    if (str_length > 0 && line[str_length - 1] == '\n')
+    {
+        line[str_length - 1] = '\0';
+    }
+
+    char *token;
+    int index = 0;
+    token = strtok(line, " ");
+
+    while (token != NULL)
+    {
+        printf("%d: %s\n", index++, token);
+        token = strtok(NULL, " ");
+    }
+}
